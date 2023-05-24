ccf83d35378a70f56fde623eb4e2f361b2940810
Cory Powell <coryrpowell@gmail.com>
2023-05-24 10:10:49 -0400

    Add comment outs

--- comparisons.rb
+++ comparisons.rb
@@ -60,32 +60,32 @@ puts "Is number_students less than or equal to 21?", number_students <= 21
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-This is printing that 4 is less than 9. The comparison operator in this is < or less than.
+# This is printing that 4 is less than 9. The comparison operator in this is < or less than.
 #YOU DO: Explain.
 
 books = 3
 puts 4 < books
-Line 66 is showing that the variable `books` is assigned the integer 3. The assignment operator is `=`.
-Line 67 is printing that 4 is less than `books` which would have the outcome of `false`. 
-To have the outcome `true` the code would need to be `4 > books`
+# Line 66 is showing that the variable `books` is assigned the integer 3. The assignment operator is `=`.
+# Line 67 is printing that 4 is less than `books` which would have the outcome of `false`. 
+# To have the outcome `true` the code would need to be `4 > books`
 # YOU DO: Explain.
 
 friends = 6
 siblings = 2
 puts friends > siblings
-Line 73 is assigning the variable `friends` is assigned the integer 6. The assignment operator is `=`.
-Line 74 is assigning the variable `siblings` is assigned the integer = 2. The assignment operator is `=`.
-Line 75 is printing that `friends` is greater than `siblings` which would have the outcome `true`, as 6 > 2.
+# Line 73 is assigning the variable `friends` is assigned the integer 6. The assignment operator is `=`.
+# Line 74 is assigning the variable `siblings` is assigned the integer = 2. The assignment operator is `=`.
+# Line 75 is printing that `friends` is greater than `siblings` which would have the outcome `true`, as 6 > 2.
 
 # YOU DO: Explain.
 
 attendees = 9
 meals = 8
 puts attendees != meals
-Line 82 is assigning the variable `attendees` the integer 9. The assignment operator is `=`.
-Line 83 is assigning the variable `meals` the integer 8. The assignment operator is `=`.
-Line 84 is printing that `attendees` is not equal to `meals` with the comparison operator `!=`.
-This puts would have the outcome `true` as 9 and 8 are not equal.
+# Line 82 is assigning the variable `attendees` the integer 9. The assignment operator is `=`.
+# Line 83 is assigning the variable `meals` the integer 8. The assignment operator is `=`.
+# Line 84 is printing that `attendees` is not equal to `meals` with the comparison operator `!=`.
+# This puts would have the outcome `true` as 9 and 8 are not equal.
 # YOU DO: Explain.
 
 
@@ -125,5 +125,5 @@ puts loves_to_play && age <= 1
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER: 
-The final code evaluated to true because the variable `loves to play` is true.
-The age of the dog is 1 and is less than or equal to 1. The assumption of `puppy` would be less than a year old.
+# The final code evaluated to true because the variable `loves to play` is true.
+# The age of the dog is 1 and is less than or equal to 1. The assumption of `puppy` would be less than a year old.
